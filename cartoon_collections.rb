f80262408d93fda15de94c1b19b9d0e84475971a
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-15 14:14:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,9 @@ def roll_call_dwarves(array)
   puts "#{number}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
   # Your code here
+  
 end
 
 def long_planeteer_calls# code an argument here
