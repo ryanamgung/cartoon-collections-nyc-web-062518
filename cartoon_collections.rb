447efc00a07e7a1d8b40f677c06cecdcc8fc43fd
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-15 14:25:10 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,9 @@ def summon_captain_planet(array)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.include do |call|
+    call.length 
 end
 
 def find_the_cheese# code an argument here
