fba46709aaecbf71e52e0ba3eb89eb07d09ba09c
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-15 14:34:19 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,8 +21,8 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   var = 0
   until var == cheese_types.length
-    if array.include?(cheese_types[i])
-      return cheese_types[i]
+    if array.include?(cheese_types[var])
+      return cheese_types[var]
     end
     var += 1
   end
