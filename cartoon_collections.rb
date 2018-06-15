b298ccbc274c076e44fb670433c753eb459c1711
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-15 14:28:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,14 +12,14 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.include do |call|
+  array.include? do |call|
     call.length >= 4
   end
 end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include do |food|
+  array.include? do |food|
     var = 0
     until var == cheese_types.length do
       food == cheese_types[var]
